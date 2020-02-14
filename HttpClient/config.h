fd1d2ffea8097de66c9448d31616d79dbe2f6eb6
Yongkun Li <indusdesign@163.com>
2020-02-14 01:03:59 -0500

    unused projects removed

--- HttpClient/config.h
+++
@@ -1,7 +0,0 @@
-const char WIFI_SSID[] = "sandbox370";
-const char WIFI_PASSWORD[] = "+s0a+s03!2gether?";
-
-const char SERVER_ADDRESS[] = "10.18.111.85";
-const int  SERVER_PORT      = 10000;
-
-const char DEVICE_ID[]      = "device_14";
