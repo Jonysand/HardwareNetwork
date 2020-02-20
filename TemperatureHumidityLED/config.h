f42d117575f8313399f0ec521e95657ae2999ad0
Yongkun Li <indusdesign@163.com>
2020-02-20 11:46:42 -0500

    config.h removed

--- TemperatureHumidityLED/config.h
+++
@@ -1,9 +0,0 @@
-const char WIFI_SSID[] = "sandbox370";
-const char WIFI_PASSWORD[] = "+s0a+s03!2gether?";
-
-const char MQTT_BROKER[] = "itpdtd.com";
-const int MQTT_PORT = 8883;
-const char MQTT_USER[] = "device_14";
-const char MQTT_PASSWORD[] = "request-contrary-finch";
-
-const String DEVICE_ID = "device_14";
